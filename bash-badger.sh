c28be98d3435e361d260af7647d2edd375059eda
arpbadger <arpbadger@protonmail.com>
2019-05-04 16:23:11 -0400

    Update bash-badger.sh

--- bash-badger.sh
+++ bash-badger.sh
@@ -1,14 +1,58 @@
+#! /bin/bash 
+
+#a simple CTF bash script
+
+# Help information 
 echo "For help, see the README "
 echo ""
 echo "Enter Project Name"
 read Project
 
-# Create Project Folder
-#echo "Creating Project Folder"
-#mkdir $Project 
+# Create New Project Directories 
+Create Project Folder
+echo "Creating Project Folder"
+mkdir $Project 
+touch /$Project/$Project"_arp.txt"
 
+# Arp scan for targets, add results to file 
 echo "Running arp-scan"
 arp-scan -l >> /$Project/$Project"_arp.txt"
 
 echo "arp-scan results in /local/arp.txt"
 echo "Enter the Target, for all in arp.txt, Enter 'All'"
+read Target 
+ 
+# Nmap targets
+
+
+#echo hello | grep hi ; echo $?
+
+
+# From https://linuxhint.com/read_file_line_by_line_bash/
+filename='/$Project/$Project"_arp.txt"'
+n=1
+while read line; do
+
+if grep 192 ; echo $? == 0:
+	do 
+	# for each line grep finds matches the text, save to a variable 
+# reading each line
+echo "Line No. $n : $line"
+n=$((n+1))
+done < $filename
+
+
+# Nmap targets 
+#for line in /$Project/$Project"_arp.txt":
+#do 
+#done 
+
+
+#
+#
+#
+#
+#
+#
+#
+#
