8c9330cb26f487734c0bb2ee0b29b21ee6a07822
arpbadger <arpbadger@protonmail.com>
2019-05-05 11:20:03 -0400

    Update bash-badger.sh

--- bash-badger.sh
+++ bash-badger.sh
@@ -4,34 +4,21 @@
 
 # Welcome Screen
 
-echo """  ____            _       ____            _                 
- |  _ \          | |     |  _ \          | |                
- | |_) | __ _ ___| |__   | |_) | __ _  __| | __ _  ___ _ __ 
- |  _ < / _` / __| '_ \  |  _ < / _` |/ _` |/ _` |/ _ \ '__|
- | |_) | (_| \__ \ | | | | |_) | (_| | (_| | (_| |  __/ |   
- |____/ \__,_|___/_| |_| |____/ \__,_|\__,_|\__, |\___|_|   
-                                             __/ |          
-                                            |___/           """
-
-
-# Help information 
-echo "For help, see the README "
-echo ""
-echo "Enter Project Name"
-read Project
-
+echo " Welcome to the Bash Badger"
 # Create New Project Directories 
-Create Project Folder
+echo "Enter CTF or Project Name"
+read Project
 echo "Creating Project Folder"
 mkdir $Project 
-touch /$Project/$Project"_arp.txt"
+#touch /$Project/$Project"_arp.txt"
 
 # Arp scan for targets, add results to file 
 echo "Running arp-scan"
 arp-scan -l >> /$Project/$Project"_arp.txt"
 
 echo "arp-scan results in /local/arp.txt"
-echo "Enter the Target, for all in arp.txt, Enter 'All'"
+subl /$Project/$Project"_arp.txt"
+echo "Enter the Target"
 read Target 
  
 # Nmap targets
