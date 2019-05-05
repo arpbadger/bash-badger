3965bb21b8ac27903a7c813958c2972b2d9e9151
arpbadger <arpbadger@protonmail.com>
2019-05-05 12:00:37 -0400

    Update bash-badger.sh

--- bash-badger.sh
+++ bash-badger.sh
@@ -3,8 +3,19 @@
 #a simple CTF bash script
 
 # Welcome Screen
+echo " 
 
-echo " Welcome to the Bash Badger"
+ ######                          ######                                     
+ #     #   ##    ####  #    #    #     #   ##   #####   ####  ###### #####  
+ #     #  #  #  #      #    #    #     #  #  #  #    # #    # #      #    # 
+ ######  #    #  ####  ######    ######  #    # #    # #      #####  #    # 
+ #     # ######      # #    #    #     # ###### #    # #  ### #      #####  
+ #     # #    # #    # #    #    #     # #    # #    # #    # #      #   #  
+ ######  #    #  ####  #    #    ######  #    # #####   ####  ###### #    # 
+                                                                            
+
+"
+
 # Create New Project Directories 
 echo "Enter CTF or Project Name"
 read Project
@@ -14,20 +25,40 @@ cd $Project
 #touch $Project"_arp.txt"
 touch bash-results.txt
 
+# Setup results text file 
+echo " 
+
+ ######                          ######                                     
+ #     #   ##    ####  #    #    #     #   ##   #####   ####  ###### #####  
+ #     #  #  #  #      #    #    #     #  #  #  #    # #    # #      #    # 
+ ######  #    #  ####  ######    ######  #    # #    # #      #####  #    # 
+ #     # ######      # #    #    #     # ###### #    # #  ### #      #####  
+ #     # #    # #    # #    #    #     # #    # #    # #    # #      #   #  
+ ######  #    #  ####  #    #    ######  #    # #####   ####  ###### #    # 
+                                                                            
+
+" >> bash-results.txt
+echo "  " >> bash-results.txt
+
+
 # Arp scan for targets, add results to file 
 echo "Running arp-scan"
 #arp-scan -l > /$Project/$Project"_arp.txt" 
 echo "Arp scan for"+$Project >> bash-results.txt
+echo "==============================================" >> bash-results.txt
+echo " " >> bash-results.txt
 arp-scan -l >> bash-results.txt
 echo "arp-scan results in /local/arp.txt"
 
 
 #Nmap targets 
-subl /$Project/$Project"_arp.txt"
 subl bash-results.txt
 echo "Enter the Target"
 read Target 
- 
+
+echo "Nmap scan for"+$Project >> bash-results.txt
+echo "==============================================" >> bash-results.txt
+echo " " >> bash-results.txt 
 nmap -sS -sV -A -O $Target >> bash-results.txt
 
 echo "Thanks for using the Bash Badger"
