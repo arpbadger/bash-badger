e33b92e0bfcd07175b35eb090388284d540157e6
arpbadger <arpbadger@protonmail.com>
2019-05-06 12:52:45 -0400

    Update bash-badger.sh

--- bash-badger.sh
+++ bash-badger.sh
@@ -1,9 +1,12 @@
 #! /bin/bash 
 
-#a simple CTF bash script
+# A simple CTF automation script 
 
-# Welcome Screen
+# Print Welcome Screen
 echo " Bash Badger CTF Automation Tool"
+echo " run with --help command for details"
+
+# Check if command was entered, if not display help page
 if [ "$1" == "" ];
 	then 
 	echo "You didn't supply a command" 
@@ -12,16 +15,27 @@ if [ "$1" == "" ];
 	--full : run a full scan including arp-scan, nmap, dirb 
 	--nodirb : run a full scan but do not include dirb "
 	exit
+
+# Run script based on input command 
 else 
+
+	# Open the help page
+	if [ "$1" == "--help" ];
+		echo "--full | run all 3 scans (arp-scan,nmap, dirb)"
+		echo "--nordirb | don't run dirb"
+		echo "--about| display information about the script"
+		echo "--help | dispaly this help page "
+
+	# Run a full scan 
 	if [ "$1" == "--full" ];
 		then 
+
 		# Create New Project Directories 
 		echo "Enter CTF or Project Name"
 		read Project
 		echo "Creating Project Folder"
 		mkdir $Project 
 		cd $Project
-		#touch $Project"_arp.txt"
 		touch bash-results.txt
 
 		# Setup results text file 
@@ -31,7 +45,6 @@ else
 
 		# Arp scan for targets, add results to file 
 		echo "Running arp-scan"
-		#arp-scan -l > /$Project/$Project"_arp.txt" 
 		echo "Arp scan for"$Project >> bash-results.txt
 		echo "==============================================" >> bash-results.txt
 		echo " " >> bash-results.txt
@@ -40,34 +53,34 @@ else
 		echo " " >> bash-results.txt
 
 
-		#Nmap targets 
+		# Get target from user, Nmap targets 
 		subl bash-results.txt
+		echo "Enter a target from the arp-scan results"
 		echo "Enter the Target"
 		read Target 
-
 		echo "Nmap scan for"$Project >> bash-results.txt
 		echo "==============================================" >> bash-results.txt
 		echo " " >> bash-results.txt 
 		nmap -sS -sV -A -O $Target >> bash-results.txt
 
 
-		#Dirb targets
-
+		# Dirb targets
 		echo "running dirb"
 		echo " Dirb scan for " $Project >> bash-results.txt
 		echo "==============================================" >> bash-results.txt
 		dirb 'http://'$Target -S >> bash-results.txt
 		fi
 
+	# Run without a dirb scan 
 	if [ "$1" == "--nodirb" ];
 		then 
+			
 		# Create New Project Directories 
 		echo "Enter CTF or Project Name"
 		read Project
 		echo "Creating Project Folder"
 		mkdir $Project 
 		cd $Project
-		#touch $Project"_arp.txt"
 		touch bash-results.txt
 
 		# Setup results text file 
@@ -77,7 +90,6 @@ else
 
 		# Arp scan for targets, add results to file 
 		echo "Running arp-scan"
-		#arp-scan -l > /$Project/$Project"_arp.txt" 
 		echo "Arp scan for"$Project >> bash-results.txt
 		echo "==============================================" >> bash-results.txt
 		echo " " >> bash-results.txt
@@ -86,84 +98,18 @@ else
 		echo " " >> bash-results.txt
 
 
-		#Nmap targets 
+		# Get target from user, Nmap targets 
 		subl bash-results.txt
+		echo "Enter a target from the arp-scan results"
 		echo "Enter the Target"
 		read Target 
-
 		echo "Nmap scan for"$Project >> bash-results.txt
 		echo "==============================================" >> bash-results.txt
 		echo " " >> bash-results.txt 
 		nmap -sS -sV -A -O $Target >> bash-results.txt
 
-
-		#Dirb targets
-		echo "Do you Want to Run Dirb (1 for yes, 0 for no) "
-		read webscan
-		if [ $webscan -gt 0 ]; then
-			echo "running dirb"
-			echo " Dirb scan for " $Project >> bash-results.txt
-			echo "==============================================" >> bash-results.txt
-			dirb 'http://'$Target -S >> bash-results.txt
-			fi
+# Exit the script 
 else 
 	echo "Thanks for runing bash-badger"
+	echo "for help, enter --help"
 	fi
-#cho "running Dirb on server"
-#b http://
-
-echo "Thanks for using the Bash Badger"
-fi
-
-# Create New Project Directories 
-echo "Enter CTF or Project Name"
-read Project
-echo "Creating Project Folder"
-mkdir $Project 
-cd $Project
-#touch $Project"_arp.txt"
-touch bash-results.txt
-
-# Setup results text file 
-echo " Bash Badger CTF Automation Tool" >> bash-results.txt
-echo "  " >> bash-results.txt
-
-
-# Arp scan for targets, add results to file 
-echo "Running arp-scan"
-#arp-scan -l > /$Project/$Project"_arp.txt" 
-echo "Arp scan for"$Project >> bash-results.txt
-echo "==============================================" >> bash-results.txt
-echo " " >> bash-results.txt
-arp-scan -l >> bash-results.txt
-echo "arp-scan results in /local/arp.txt"
-echo " " >> bash-results.txt
-
-
-#Nmap targets 
-subl bash-results.txt
-echo "Enter the Target"
-read Target 
-
-echo "Nmap scan for"$Project >> bash-results.txt
-echo "==============================================" >> bash-results.txt
-echo " " >> bash-results.txt 
-nmap -sS -sV -A -O $Target >> bash-results.txt
-
-
-#Dirb targets
-echo "Do you Want to Run Dirb (1 for yes, 0 for no) "
-read webscan
-if [ $webscan -gt 0 ]; then
-	echo "running dirb"
-	echo " Dirb scan for " $Project >> bash-results.txt
-	echo "==============================================" >> bash-results.txt
-	dirb 'http://'$Target -S >> bash-results.txt
-	fi
-else 
-	echo "Thanks for runing bash-badger"
-	fi
-#cho "running Dirb on server"
-#b http://
-
-#echo "Thanks for using the Bash Badger"
